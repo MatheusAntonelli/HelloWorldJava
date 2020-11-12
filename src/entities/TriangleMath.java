c680d35c25ee1bf6fca6d74abe06488eec8ccf22
Matheus Antonelli <matheusantonelli12@gmail.com>
2020-11-12 20:35:01 -0300

    Add TriangleMath.java to get the area of a Triangle

---
+++ src/entities/TriangleMath.java
@@ -0,0 +1,10 @@
+package entities;
+
+public class TriangleMath {
+    public double A,B,C;
+
+    public double area(){
+        double p = (A + B + C ) / 2;
+        return Math.sqrt(p * (p - A) * (p - B) * (p - C));
+    }
+}
