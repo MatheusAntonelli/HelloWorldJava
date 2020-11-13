a049c28deb432942fb852d72caf98883c0c13c23
Matheus Antonelli de Oliveira <matheusantonelli12@gmail.com>
2020-11-13 16:27:18 -0300

    Add IOFCalc.java That is class for calculate the IOF to buy Dollars in Brazil!

---
+++ src/util/IOFCalc.java
@@ -0,0 +1,25 @@
+package util;
+
+public class IOFCalc {
+
+    public double dollarPrice;
+    public double neededDollars;
+
+    public double finalPrice(){
+        return neededDollars * dollarPrice;
+    }
+    static double iof = 6;
+
+    public double IOFPrice(){
+        return finalPrice() * iof / 100;
+    }
+
+    public double pricePayed(){
+        return IOFPrice() + finalPrice();
+    }
+
+    public String toString(){
+        //You can format and use %.2f to show two decimals in the result
+        return "Amount to be paid in reais = R$: " + pricePayed();
+    }
+}
