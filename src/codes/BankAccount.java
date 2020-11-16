f14a46b945fd131d34116d1ae0d53331b066310a
Matheus Antonelli de Oliveira <matheusantonelli12@gmail.com>
2020-11-16 19:41:26 -0300

    Add BankAccount.java and BankCreateAccount.java

---
+++ src/codes/BankAccount.java
@@ -0,0 +1,47 @@
+package codes;
+
+import util.BankCreateAccount;
+
+import java.util.Scanner;
+
+public class BankAccount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the account number: ");
+        int numberAccount = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Holder name: ");
+        String holder = sc.nextLine();
+
+        System.out.print("Do you want to make an initial deposit? (Y/N)");
+        String makeDeposit = sc.nextLine();
+        double deposit;
+        if (makeDeposit.equals("y") || makeDeposit.equals("Y")){
+            System.out.print("Amount of deposit:\nU$: ");
+            deposit = sc.nextDouble();
+
+        }else{
+            System.out.println();
+            System.out.println("Ok, you can deposit later!");
+            deposit = 0;
+        }
+        BankCreateAccount holderOne = new BankCreateAccount(numberAccount, holder, deposit);
+        System.out.println(holderOne.opening());
+        System.out.println();
+
+
+        System.out.print("Add money?\nU$: ");
+        double addMoney = sc.nextDouble();
+        holderOne.addBalance(addMoney);
+        System.out.println(holderOne);
+
+
+        System.out.print("Withdraw: \nU$:");
+        double withdrawMoney = sc.nextDouble();
+        holderOne.removeBalance(withdrawMoney);
+        System.out.println("Money Withdraw sucesfully!");
+
+        System.out.println(holderOne);
+    }
+}
---
+++ src/util/BankCreateAccount.java
@@ -0,0 +1,61 @@
+package util;
+
+public class BankCreateAccount {
+    private  int accountNumber;
+    private String accountHolder;
+    private double initialDeposit;
+    private double secondDeposit;
+
+    public BankCreateAccount(int accountNumber, String accountHolder, double initialDeposit) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.initialDeposit = initialDeposit;
+    }
+
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+
+    public void setAccountHolder(String accountHolder) {
+        this.accountHolder = accountHolder;
+    }
+
+
+    public void setInitialDeposit(double initialDeposit){
+
+       this.initialDeposit = initialDeposit;
+    }
+
+
+    public void addBalance(double balance){
+        this.secondDeposit += balance;
+    }
+    public void removeBalance(double balance){
+        this.secondDeposit -= balance - 5;
+    }
+
+
+    private double finalBalance(){
+        return secondDeposit + initialDeposit;
+    }
+
+    public String opening(){
+        return "       INFO     \n" +
+                "Account Number: " + accountNumber
+                +"\n" + "Acount Holder: " + accountHolder
+                +"\n" +"Initial Deposit: " +  initialDeposit;
+    }
+
+
+    @Override
+    public String toString(){
+        return "    INFO     \n" +
+                "Account Number: " + accountNumber
+                +"\n" + "Acount Holder: " + accountHolder
+                +"\n" + "Balance: " + finalBalance();
+    }
+
+
+}
